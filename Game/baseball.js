e427ffe56babb95675a8414c6e5f194a9054c615
eunjin0212 <eunjin9639@gmail.com>
2020-09-23 21:57:05 +0900

    strike

--- Game/baseball.js
+++ Game/baseball.js
@@ -4,9 +4,12 @@ const bbResult = document.querySelector(".bs-result"),
   bbBtn = bbForm.querySelector(".bs-btn");
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let numberArr = [];
-bbInput.maxLength = 4;
-//bbInput.type = "text";
-//bbInput.type = Number;
+
+function maxLengthCheck(object) {
+  if (object.value.length > object.maxLength) {
+    object.value = object.value.slice(0, object.maxLength);
+  }
+} //숫자 제한하기
 
 function numFunc() {
   //랜덤 숫자 만들기
@@ -18,12 +21,14 @@ function numFunc() {
     //하나씩 줄어드는 숫자를 표현되게 i를 빼줌
     //splice 배열의 요소를 변경할 때 사용
     numberArr.push(pickNumber[0]); //배열의 첫번째 자리를 추출
-    console.log(pickNumber);
   }
-  console.log(numberArr);
+  console.log("문제", numberArr);
+  console.log(typeof numberArr); //object
 }
+let result = bbInput.value;
+
 function resetNum() {
-  bbInput.value = "";
+  result = "";
 }
 
 numFunc();
@@ -32,8 +37,6 @@ let wrongAnswer = 0;
 
 bbForm.addEventListener("submit", function (event) {
   event.preventDefault(); //새로고침 막아주기
-  const result = bbInput.value;
-  console.log(bbInput.value);
   if (result === numberArr) {
     //홈런인지 아닌지
     bbResult.textContent = "홈런";
@@ -43,12 +46,13 @@ bbForm.addEventListener("submit", function (event) {
   } else {
     //몇번 스트라이크, 볼을 했는지
     const resultArr = result;
+    console.log(typeof result); //string
     let strike = 0;
     let ball = 0;
     wrongAnswer++;
     if (wrongAnswer > 3) {
       //정답 횟수 제한
-      bbResult.textContent = "한도초과! 답은" + numberArr.join(",");
+      bbResult.textContent = "한도초과! 답은" + numberArr.join("");
       //join은 "",""... 이런식으로 합쳐줌
       numFunc();
       resetNum();
@@ -58,15 +62,15 @@ bbForm.addEventListener("submit", function (event) {
         if (Number(resultArr[i]) === numberArr[i]) {
           //타입을 숫자로 변환
           strike++;
-        } else if (numberArr.indexOf(Number(resultArr[i])) > -1) {
+        } else if (numberArr.indexOf(Number(resultArr[i]))) {
           //배열 안에 일치하는 것 찾기
           ball++;
         }
       }
       bbResult.textContent = strike + "스트라이크" + ball + "볼";
       resetNum();
-      c;
-      console.log(resultArr);
+
+      console.log("resultArr", resultArr);
     }
   }
 });

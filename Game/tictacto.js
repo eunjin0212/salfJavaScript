798c0e59c8c7b8a9bc8aa797f5690481afba9b8e
eunjin0212 <eunjin9639@gmail.com>
2020-09-24 16:09:06 +0900

    tictacto

---
+++ Game/tictacto.js
@@ -0,0 +1,4 @@
+const ttable = document.querySelector(".table"),
+  row = document.querySelectorAll(".table-row"),
+  can = document.querySelectorAll(".can"),
+  resultBox = document.querySelector("result-box"),

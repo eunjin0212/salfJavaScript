3d6b8a01e136a5f59ce6116dbb9eb8cc5db62193
eunjin0212 <eunjin9639@gmail.com>
2020-09-24 17:40:29 +0900

    user

--- Game/tictacto.js
+++ Game/tictacto.js
@@ -2,3 +2,5 @@ const ttable = document.querySelector(".table"),
   row = document.querySelectorAll(".table-row"),
   can = document.querySelectorAll(".can"),
   resultBox = document.querySelector("result-box"),
+  me = "x",
+  you = "o";
